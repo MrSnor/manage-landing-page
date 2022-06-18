e6151129b1211b379900b9670d42c0cb8f29e6e7
Partha Priyadarshi Das <parthap.das36@gmail.com>
2022-06-18 09:28:41 +0530

    Update mobile nav menu
    remove scroll when menu is visible and change menu overlay bg

--- js/toggleMenu.js
+++ js/toggleMenu.js
@@ -9,9 +9,11 @@ navToggle.addEventListener("click", () => {
   if (visiblity === "false") {
     nav.setAttribute("data-visible", "true")
     navToggle.setAttribute("aria-expanded", "true")
+    document.body.style.overflowY = "hidden";
   }
   // if navbar is visible
   else {
+    document.body.style.overflowY = "auto";
     nav.setAttribute("data-visible", "false")
     navToggle.setAttribute("aria-expanded", "false")
   }
@@ -19,6 +21,7 @@ navToggle.addEventListener("click", () => {
 
 // to close menu when it(menu button (havent been perfected)) loses focus
 navToggle.addEventListener('blur', () => {
+  document.body.style.overflowY = "auto";
   nav.setAttribute("data-visible", "false")
   navToggle.setAttribute("aria-expanded", "false")
 })
